6549540f9ade08004b2febef7f5455700407b65d
Dalton Boll <daltonboll@gmail.com>
2016-04-19 21:41:14 -0700

    Add phone_number attribute to Users

--- PensieveServer/db/schema.rb
+++ PensieveServer/db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420042629) do
+ActiveRecord::Schema.define(version: 20160420043759) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password"
     t.integer  "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "phone_number"
   end
 
 end
